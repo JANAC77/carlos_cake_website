775cb99dcef7ecf30933e7f12d8733921b627f75
Janarthanan <janarthananc01@gmail.com>
2026-05-26 14:48:41 +0530

    mobile number changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
   };
 
   const contactInfo = [
-    { icon: FaPhone, title: 'Phone', details: ['+91 81477 51838 ', '+91 77953 22889'], color: 'text-green-500' },
+    { icon: FaPhone, title: 'Phone', details: ['+91 81477 51838 ', '+91 98809 44843'], color: 'text-green-500' },
     { icon: FaEnvelope, title: 'Email', details: ['carloscakecafe26@gmail.com'], color: 'text-blue-500' },
     { icon: FaMapMarkerAlt, title: 'Address', details: ['#3, Bellandur Gate, Sarjapur Road', 'Near Spencer Hyper Market, Bangalore-560068'], color: 'text-red-500' },
     { icon: FaClock, title: 'Business Hours', details: ['Monday - Sunday: 9:00 AM - 10:00 PM'], color: 'text-purple-500' }
